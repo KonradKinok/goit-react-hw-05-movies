ae63cf849a129c01a84d62d174d203c84d3f4147
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-11-21 09:42:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "",
-//   build: {
-//     outDir: "./dist",
-//   },
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "",
+  build: {
+    outDir: "./dist",
+  },
+});
 
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
 
 // <https://vitejs.dev/config/>
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
